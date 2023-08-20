67d2ebbb6f851ce72403727d700d69e3f6eb2ac8
alfredexertier <alfred.exertier@hotmail.fr>
2023-08-20 20:18:14 +0200

    done

--- app.py
+++ app.py
@@ -1,12 +1,71 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import lightgbm as lgb
+import shap
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
+model = lgb.Booster(model_file='final_model.txt')
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+df = pd.read_csv('df_final_1000.csv')
+df.drop('Unnamed: 0', axis=1, inplace=True)
+print(df.shape)
+for col in df.columns:
+    print(col)
+    try:
+        df[col] = df[col].astype(float)
+    except Exception as e:
+        print(f"Error with column {col}: {e}")
 
+explainer = shap.TreeExplainer(model)
+
+
+@app.route('/predict/<int:row_num>', methods=['GET'])
+def predict(row_num):
+    if row_num < 0 or row_num >= len(df):
+        return jsonify({"error": "Invalid row number"}), 400
+
+    sample = df.iloc[row_num, :]
+
+
+
+    sample_df = sample.to_frame().T
+
+    prediction = model.predict(sample_df)[0]
+    shap_values = explainer.shap_values(sample_df)
+
+    if isinstance(shap_values, list):
+        shap_values = shap_values[1]
+
+    top_features_idx = np.argsort(np.abs(shap_values[0]))[-3:]
+    top_features = sample_df.columns[top_features_idx].tolist()
+
+    user_data = {}
+    for col in top_features:
+        user_data[col] = sample[col]
+    print(user_data)
+    return jsonify({
+        "prediction": bool(prediction > 0.5),
+        "top_features": top_features,
+        "user_data": user_data
+    })
+
+
+@app.route('/get_data_for_features', methods=['GET'])
+def get_data_for_features():
+    feature_0 = request.args.get('feature_0')
+    feature_1 = request.args.get('feature_1')
+    feature_2 = request.args.get('feature_2')
+
+    if not all([feature_0, feature_1, feature_2]):
+        return jsonify({"error": "All three features must be provided!"}), 400
+
+    try:
+        raw_data = df[[feature_0, feature_1, feature_2]].to_dict(orient='list')
+    except KeyError:
+        return jsonify({"error": "One or more features are invalid!"}), 400
+    return jsonify(raw_data)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8000, debug=True)
--- test_app.py
+++ test_app.py
@@ -1,8 +1,72 @@
 from app import app
 
+def test_invalid_row_num():
+    response = app.test_client().get('/predict/1000000000')  # Test with row number as one billion
 
-def test_hello():
-    response = app.test_client().get('/')
+    assert response.status_code == 400
+    assert b"Invalid row number" in response.data
+
+
+def test_response_format():
+    # Assuming you have a valid row number 0, adjust as needed.
+    response = app.test_client().get('/predict/0')
 
     assert response.status_code == 200
-    assert response.data == b'Hello World!'
+
+    # Convert the byte data to JSON
+    data = response.get_json()
+
+    # Check if the response has the expected keys
+    assert 'prediction' in data
+    assert 'top_features' in data
+    assert 'user_data' in data
+
+    # Check if prediction is a boolean
+    assert isinstance(data['prediction'], bool)
+
+    # Check if top_features is a list containing three items
+    assert isinstance(data['top_features'], list)
+    assert len(data['top_features']) == 3
+
+    # Check if user_data is a dictionary and contains the correct keys (from top_features)
+    assert isinstance(data['user_data'], dict)
+    for feature in data['top_features']:
+        assert feature in data['user_data']
+def test_missing_feature():
+    response = app.test_client().get('/get_data_for_features',
+                                     query_string={'feature_0': 'CODE_GENDER', 'feature_1': 'FLAG_OWN_CAR'})
+    # Here, we are intentionally leaving out FLAG_OWN_REALTY
+
+    assert response.status_code == 400
+    assert b"All three features must be provided!" in response.data
+
+
+def test_invalid_feature():
+    response = app.test_client().get('/get_data_for_features', query_string={
+        'feature_0': 'CODE_GENDER',
+        'feature_1': 'FLAG_OWN_CAR',
+        'feature_2': 'thats not working'
+    })
+
+    assert response.status_code == 400
+    assert b"One or more features are invalid!" in response.data
+
+
+def test_successful_data_retrieval():
+    response = app.test_client().get('/get_data_for_features', query_string={
+        'feature_0': 'CODE_GENDER',
+        'feature_1': 'FLAG_OWN_CAR',
+        'feature_2': 'FLAG_OWN_REALTY'
+    })
+
+    assert response.status_code == 200
+
+    # Convert the byte data to JSON
+    data = response.get_json()
+
+    # Ensure all the lists in the returned data contain only numbers (int or float)
+    for key, value in data.items():
+        assert all(isinstance(item, (int, float)) for item in value)
+
+
+

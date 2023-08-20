8ad51d7653ec3200070dce934ce64c29acfaf8ae
alfredexertier <alfred.exertier@hotmail.fr>
2023-08-21 00:56:46 +0200

    update pipeline

--- test_app.py
+++ test_app.py
@@ -8,34 +8,27 @@ def test_invalid_row_num():
 
 
 def test_response_format():
-    # Assuming you have a valid row number 0, adjust as needed.
     response = app.test_client().get('/predict/0')
 
     assert response.status_code == 200
 
-    # Convert the byte data to JSON
     data = response.get_json()
 
-    # Check if the response has the expected keys
     assert 'prediction' in data
     assert 'top_features' in data
     assert 'user_data' in data
 
-    # Check if prediction is a boolean
     assert isinstance(data['prediction'], bool)
 
-    # Check if top_features is a list containing three items
     assert isinstance(data['top_features'], list)
     assert len(data['top_features']) == 3
 
-    # Check if user_data is a dictionary and contains the correct keys (from top_features)
     assert isinstance(data['user_data'], dict)
     for feature in data['top_features']:
         assert feature in data['user_data']
 def test_missing_feature():
     response = app.test_client().get('/get_data_for_features',
                                      query_string={'feature_0': 'CODE_GENDER', 'feature_1': 'FLAG_OWN_CAR'})
-    # Here, we are intentionally leaving out FLAG_OWN_REALTY
 
     assert response.status_code == 400
     assert b"All three features must be provided!" in response.data
@@ -61,10 +54,8 @@ def test_successful_data_retrieval():
 
     assert response.status_code == 200
 
-    # Convert the byte data to JSON
     data = response.get_json()
 
-    # Ensure all the lists in the returned data contain only numbers (int or float)
     for key, value in data.items():
         assert all(isinstance(item, (int, float)) for item in value)
 
